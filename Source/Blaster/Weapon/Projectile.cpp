a06191644211e0225b23c1a47f51b77a333fd355
resh7007 <resh87@mail.ru>
2022-07-05 18:48:11 +0600

    added projectile hit events

--- Source/Blaster/Weapon/Projectile.cpp
+++ Source/Blaster/Weapon/Projectile.cpp
@@ -7,6 +7,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Particles/ParticleSystemComponent.h"
 #include "Particles/ParticleSystem.h"
+#include "Sound/SoundCue.h"
 AProjectile::AProjectile()
 {
   	PrimaryActorTick.bCanEverTick = true;
@@ -41,6 +42,17 @@ void AProjectile::BeginPlay()
 			EAttachLocation::KeepWorldPosition
 		);
 	}
+
+	if(HasAuthority())
+	{
+		CollisionBox->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+	}
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+
+	Destroy();
 }
  
 void AProjectile::Tick(float DeltaTime)
@@ -48,4 +60,15 @@ void AProjectile::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
+void AProjectile::Destroyed()
+{
+	Super::Destroyed();
+	if(ImpactParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticles,GetActorTransform());
+	}
+	if(ImpactSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound,GetActorLocation());
+	}
+}
--- Source/Blaster/Weapon/Projectile.h
+++ Source/Blaster/Weapon/Projectile.h
@@ -14,10 +14,12 @@ class BLASTER_API AProjectile : public AActor
 public:	 
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
-
+	virtual void Destroyed() override;
 protected: 
 	virtual void BeginPlay() override;
-
+	
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 private:
 	UPROPERTY(EditAnywhere)
 	class UBoxComponent* CollisionBox;
@@ -29,6 +31,12 @@ private:
 	class UParticleSystem* Tracer;
 
 	class UParticleSystemComponent* TracerComponent;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
 public:	 
 
 };
