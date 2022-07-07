a7adeba5bcc818af243e96c6bcd7887939ec7f08
resh7007 <resh87@mail.ru>
2022-07-07 14:41:23 +0600

    crosshair spreading dynamically based on jumping and running

--- Source/Blaster/HUD/BlasterHUD.h
+++ Source/Blaster/HUD/BlasterHUD.h
@@ -16,6 +16,7 @@ public:
 	UTexture2D* CrosshairsRight;
 	UTexture2D* CrosshairsTop;
 	UTexture2D* CrosshairsBottom; 
+	float CrosshairSpread;
 }; 
 /**
  * 
@@ -28,7 +29,10 @@ public:
 	virtual void DrawHUD() override;
 private:
 	FHUDPackage HUDPackage;
-	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter);
+	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter, FVector2D Spread);
+
+	UPROPERTY(EditAnywhere);
+	float CrosshairSpreadMax = 16.f;
 public:
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& Package) { HUDPackage = Package;}
 
