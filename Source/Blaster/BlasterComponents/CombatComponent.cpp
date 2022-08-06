cb6c0682cfbf7bb9fd3cf73daacf18463fdadf3d
resh7007 <resh87@mail.ru>
2022-08-06 17:56:51 +0600

    auto reloading weapon

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -111,7 +111,10 @@ void UCombatComponent::FireTimerFinished()
 	{
 		Fire();
 	}
-
+	if(EquippedWeapon-> IsEmpty())
+	{
+		Reload();
+	}
 }
 
 void UCombatComponent::ServerFire_Implementation(const FVector_NetQuantize& TraceHitTarget)
@@ -167,6 +170,11 @@ void UCombatComponent::EquipWeapon(AWeapon* WeaponToEquip)
 			Character->GetActorLocation()
 		);
 	}
+
+	if(EquippedWeapon-> IsEmpty())
+	{
+		Reload();
+	}
 	Character->GetCharacterMovement()->bOrientRotationToMovement = false;
 	Character->bUseControllerRotationYaw = true;
 															
@@ -200,7 +208,11 @@ void UCombatComponent::FinishReloading()
 	}
 
 	if(bFireButtonPressed)
+	{	
 		Fire();
+	}
+	
+
 	
 }
 void UCombatComponent::UpdateAmmoValues()
