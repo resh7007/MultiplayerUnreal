da7335029ad8b7551795ec5ac3602c0948332d39
resh7007 <resh87@mail.ru>
2022-08-15 08:58:42 +0600

    time sync

--- Source/Blaster/PlayerController/BlasterPlayerController.cpp
+++ Source/Blaster/PlayerController/BlasterPlayerController.cpp
@@ -30,6 +30,19 @@
  {
     Super::Tick(DeltaTime);
     SetHUDTime();
+
+    CheckTimeSync(DeltaTime);
+
+ }
+
+ void ABlasterPlayerController::CheckTimeSync(float DeltaTime)
+ {
+    TimeSyncRunningTime += DeltaTime;
+    if(IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+    {
+      ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+      TimeSyncRunningTime = 0.f;
+    }
  }
 
  void ABlasterPlayerController::SetHUDHealth(float Health, float MaxHealth)
@@ -158,11 +171,40 @@ void ABlasterPlayerController::SetHUDMatchCountdown(float CountdownTime)
 
 void ABlasterPlayerController::SetHUDTime()
 {
-    uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetWorld()->GetTimeSeconds());
+    uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetServerTime());
   if(CountDownInt != SecondsLeft)
   {
-    SetHUDMatchCountdown(MatchTime - GetWorld()->GetTimeSeconds());
+    SetHUDMatchCountdown(MatchTime - GetServerTime());
   }
 
     CountDownInt = SecondsLeft;
 }
+void ABlasterPlayerController::ServerRequestServerTime_Implementation(float TimeOfCLientRequest)
+{
+  float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+  ClientReportServerTime(TimeOfCLientRequest, ServerTimeOfReceipt);
+}
+void ABlasterPlayerController::ClientReportServerTime_Implementation(float TimeOfCLientRequest, float TimeServerReceivedClientRequest)
+{
+  float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfCLientRequest;
+  float CurrentServerTime = TimeServerReceivedClientRequest + (0.5f * RoundTripTime);
+
+  ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds(); 
+}
+
+
+ float ABlasterPlayerController::GetServerTime()
+ {
+  if(HasAuthority()) return  GetWorld()->GetTimeSeconds();
+  else return  GetWorld()->GetTimeSeconds() + ClientServerDelta;
+ }
+
+void  ABlasterPlayerController::ReceivedPlayer()
+{
+  Super::ReceivedPlayer();
+  if(IsLocalController())
+  {
+    ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+  }
+}
+ 
--- Source/Blaster/PlayerController/BlasterPlayerController.h
+++ Source/Blaster/PlayerController/BlasterPlayerController.h
@@ -25,9 +25,30 @@ public:
 
 	virtual void OnPossess(APawn* InPawn) override; 
 	virtual void Tick(float DeltaTime) override;
+
+	virtual float GetServerTime();// Synced with server world clock
+	virtual void ReceivedPlayer() override; // sync with server clock as soon as possible
 protected:
  	virtual void BeginPlay() override;
 	void SetHUDTime();
+
+	/**
+	 *  Sync time between client and server
+	 */
+	//Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfCLientRequest);
+
+	// reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Server, Reliable)
+	void ClientReportServerTime(float TimeOfCLientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f; // difference between client and server time
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 9.f;
+
+	float TimeSyncRunningTime = 0.f;
+	void CheckTimeSync(float DeltaTime);
 private:
 	UPROPERTY()
 	class ABlasterHUD* BlasterHUD;
